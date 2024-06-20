8e8a74a227d8bccac24329f12f99ac08e7806263
Aziz Dadoboev <aziz.bmstu@gmail.com>
2024-06-20 23:13:25 +0300

    count done tasks and display

--- app/src/main/java/com/yms/mind/TodoItemsFragment.kt
+++ app/src/main/java/com/yms/mind/TodoItemsFragment.kt
@@ -1,10 +1,12 @@
 package com.yms.mind
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.yms.mind.data.TodoItemsRepository
@@ -25,14 +27,14 @@ class TodoItemsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_todo_items, container, false)
-
         val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        val subtitle = view.findViewById<TextView>(R.id.tasks_done)
 
         recyclerView.layoutManager = LinearLayoutManager(context)
-
         adapter = TaskAdapater()
         adapter.data = repository.getTodoItems()
         recyclerView.adapter = adapter
+        subtitle.text = getString(R.string.subtitle, repository.getDoneCount())
 
         return view
     }
--- app/src/main/java/com/yms/mind/data/TodoItemsRepository.kt
+++ app/src/main/java/com/yms/mind/data/TodoItemsRepository.kt
@@ -2,17 +2,29 @@ package com.yms.mind.data
 
 class TodoItemsRepository {
     private val todoItems = mutableListOf<TodoItem>()
+    private val longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
     init {
         todoItems.add(TodoItem("1", "Buy groceries", Priority.NORMAL, "2022-09-30", false, "2022-09-27", null))
-        todoItems.add(TodoItem("2", "Finish report", Priority.HIGH, "2022-10-05", false, "2022-09-27", null))
-        todoItems.add(TodoItem("3", "Call mom", Priority.LOW, "null", false, "2022-09-28", null))
+        todoItems.add(TodoItem("2", "Finish report blah blah  blah blah  blah blah  blah blah", Priority.HIGH, "2022-10-05", false, "2022-09-27", null))
+        todoItems.add(TodoItem("3", "Call mom", Priority.LOW, "null", true, "2022-09-28", null))
         todoItems.add(TodoItem("4", "Go for a run", Priority.NORMAL, "2022-09-29", false, "2022-09-27", null))
         todoItems.add(TodoItem("5", "Read a book", Priority.LOW, null, false, "2022-09-28", null))
+        todoItems.add(TodoItem("6", longText, Priority.LOW, null, false, "2022-09-28", null))
     }
 
     fun getTodoItems(): List<TodoItem> {
         return todoItems
+    }
+
+    fun getDoneCount(): Int {
+        var count = 0
+        for (item in todoItems) {
+            if (item.status) {
+                count++
+            }
+        }
+        return count
     }
 
     fun addTodoItem(todoItem: TodoItem) {
