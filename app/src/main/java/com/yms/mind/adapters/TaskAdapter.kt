2a4b93277ddc5fd3089c479c0c50da0527f27c05
Aziz Dadoboev <aziz.bmstu@gmail.com>
2024-06-28 16:52:49 +0300

    item priority icons added

--- app/src/main/java/com/yms/mind/adapters/TaskAdapter.kt
+++ app/src/main/java/com/yms/mind/adapters/TaskAdapter.kt
@@ -4,9 +4,12 @@ import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.checkbox.MaterialCheckBox
 import com.yms.mind.R
 import com.yms.mind.data.Priority
 import com.yms.mind.data.TodoItem
@@ -37,42 +40,74 @@ class TaskAdapter(
             itemClickListener: OnItemClickListener
         ) {
             with(binding) {
+                // TodoItem Text
                 checkText.text = task.text
                 if (task.status) {
                     checkText.paintFlags = checkText.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
                 }
 
-                checkBox.isChecked = task.status
+                // TodoItem priority Icon
+                if (!task.status) setupPriorityIcon(task, priorityIcon)
+                else priorityIcon.visibility = View.GONE
 
-//                checkBox.setOnCheckedChangeListener(null)
-                checkBox.setOnCheckedChangeListener { _, isChecked ->
-                    if (isChecked) {
-                        checkText.paintFlags = checkText.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-                    } else {
-                        checkText.paintFlags = checkText.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
-                    }
-                }
-                checkBox.setOnClickListener {
-                    checkBoxClickListener.onCheckBoxClicked(task.id, checkBox.isChecked)
-                }
+                // TodoItem deadline
                 if (task.deadline != null && !task.status) {
                     dateText.text = task.deadline
-                    dateText.visibility = View.VISIBLE
                 } else {
                     dateText.visibility = View.GONE
                 }
 
-                checkBox.buttonIconDrawable = if (!task.status && task.priority == Priority.HIGH) {
-                    ContextCompat.getDrawable(checkBox.context, R.drawable.ic_unchecked_red)
-                } else if (!task.status) {
-                    ContextCompat.getDrawable(checkBox.context, R.drawable.ic_unchecked)
-                } else {
-                    ContextCompat.getDrawable(checkBox.context, R.drawable.ic_checked)
+                // TodoItem checkBox
+                setupCheckBox(checkBox, checkText, priorityIcon, task)
+                checkBox.setOnClickListener {
+                    checkBoxClickListener.onCheckBoxClicked(task.id, checkBox.isChecked)
                 }
 
                 root.setOnClickListener {
                     itemClickListener.onItemClick(task)
                 }
+            }
+        }
+
+        private fun setupPriorityIcon(
+            task: TodoItem,
+            priorityIcon: ImageView
+        ) {
+            when (task.priority) {
+                Priority.HIGH -> {
+                    priorityIcon.setImageResource(R.drawable.ic_priority_high)
+                    priorityIcon.visibility = View.VISIBLE
+                }
+                Priority.LOW -> {
+                    priorityIcon.setImageResource(R.drawable.ic_priority_low)
+                    priorityIcon.visibility = View.VISIBLE
+                }
+                else -> priorityIcon.visibility = View.GONE
+            }
+        }
+
+        private fun setupCheckBox(
+            checkBox: MaterialCheckBox,
+            checkText: TextView,
+            priorityIcon: ImageView,
+            task: TodoItem
+        ) {
+            checkBox.isChecked = task.status
+            checkBox.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    checkText.paintFlags = checkText.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                    priorityIcon.visibility = View.GONE
+                } else {
+                    checkText.paintFlags = checkText.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+                    setupPriorityIcon(task, priorityIcon)
+                }
+            }
+            checkBox.buttonIconDrawable = if (!task.status && task.priority == Priority.HIGH) {
+                ContextCompat.getDrawable(checkBox.context, R.drawable.ic_unchecked_red)
+            } else if (!task.status) {
+                ContextCompat.getDrawable(checkBox.context, R.drawable.ic_unchecked)
+            } else {
+                ContextCompat.getDrawable(checkBox.context, R.drawable.ic_checked)
             }
         }
     }
